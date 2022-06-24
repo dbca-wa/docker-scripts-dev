72f908a1f4b84f0705b425144b9f3dda12dd65fa
Jason Moore <jmoore@austwa.com>
2022-06-24 16:03:27 +0800

    Update code_server_docker.sh

--- code_server_docker.sh
+++ code_server_docker.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+docker stop code-server
+docker rm code-server
+
+docker run -d --name=code-server --env-file=env/codeserver.env -p 8443:8443 -v /container-data/code-server/config:/config -v /container-data/datamount/:/config/workspace --restart unless-stopped lscr.io/linuxserver/code-server:latest
