0c5c5368ec3acdba6a677a2be45133ced224b150
Jason Moore <jmoore@austwa.com>
2022-06-29 13:10:13 +0800

    Update ledger_docker_load.bat

--- windows/ledger_docker_load.bat
+++ windows/ledger_docker_load.bat
@@ -1,2 +1,2 @@
 docker stop ledger-dev 
-docker run --rm --name=ledger-dev --env-file=env/ledger.env --hostname ledger-dev  -v dockercontainer:/data/ -p 172.17.0.1:9021-9030:9021-9030 -p 172.17.0.1:2224:22 -p 172.17.0.1:7001:8080  -d -i -t dbcawa/docker_app_dev:ledger_dev_latest
+docker run --rm --name=ledger-dev --env-file=env/ledger.env --hostname ledger-dev  -v dockercontainer:/data/ -p 10.17.0.1:9021-9030:9021-9030 -p 10.17.0.1:2224:22 -p 10.17.0.1:7001:8080  -d -i -t dbcawa/docker_app_dev:ledger_dev_latest
