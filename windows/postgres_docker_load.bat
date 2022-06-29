e72f34047e76c9982fe69c5ec4d126af421f358c
Jason Moore <jmoore@austwa.com>
2022-06-29 12:59:20 +0800

    Update postgres_docker_load.bat

--- windows/postgres_docker_load.bat
+++ windows/postgres_docker_load.bat
@@ -1,3 +1,3 @@
 
 docker stop postgres-dev
-docker run --rm --name=postgres-dev --hostname boranga-dev -v postgresubuntudb:/data/ -p 10.17.0.1:9011-9020:9011-9020 -p 10.17.0.1:2223:22 -p 10.17.0.1:5432:5432  -d -i -t dbcawa/docker_app_dev:postgresubuntu2004_latest
+docker run --rm --name=postgres-dev --hostname postgres-dev -v postgresubuntudb:/data/ -p 10.17.0.1:9011-9020:9011-9020 -p 10.17.0.1:2223:22 -p 10.17.0.1:5432:5432  -d -i -t dbcawa/docker_app_dev:postgresubuntu2004_latest
