0a78c6f6bfa86e9f3f6c7449c4ccf0a5cbf2d6ff
Jason Moore <jmoore@austwa.com>
2022-06-29 12:32:30 +0800

    Update boranga_docker_load.bat

--- windows/boranga_docker_load.bat
+++ windows/boranga_docker_load.bat
@@ -1,3 +1,2 @@
 docker stop boranga-dev
-docker run --rm --name=boranga-dev
 docker run --rm --name=boranga-dev --env-file=env/boranga.env --hostname boranga-dev  -v dockercontainer:/data/ -p 172.17.0.1:9000-9010:9000-9010 -p 172.17.0.1:2222:22 -p 172.17.0.1:7002:8080 -d -i -t dbcawa/docker_app_dev:boranga_dev_latest
