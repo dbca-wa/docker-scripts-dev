985447f0ebf51ede256810085aec935e9df8093a
Jason Moore <jason.moore@dbca.wa.gov.au>
2022-07-04 09:13:39 +0000

    Dev Warning

--- windows_vbox/webdav_docker_load.sh
+++ windows_vbox/webdav_docker_load.sh
@@ -3,4 +3,5 @@
 docker stop webdav
 docker rm webdav
 
-docker run  --restart always -v /data:/var/lib/dav -e AUTH_TYPE=Digest -e USERNAME=docker -e PASSWORD=docker --publish 6999:80 --name webdav     -e LOCATION=/webdav -d dbcawa/docker_app_dev:webdav_dev_latest 
+# specifically for a dev environment. - not for production use.
+docker run  --restart always -v /data:/var/lib/dav -e AUTH_TYPE=Digest -e USERNAME=docker -e PASSWORD=docker --publish 6999:80 --name webdav -e LOCATION=/webdav -d dbcawa/docker_app_dev:webdav_dev_latest 
