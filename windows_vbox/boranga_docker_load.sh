ce2de8539be1fc188b457d04e2f28d12efd1e8e8
Jason Moore <jmoore@austwa.com>
2022-07-27 17:28:41 +0800

    Update boranga_docker_load.sh

--- windows_vbox/boranga_docker_load.sh
+++ windows_vbox/boranga_docker_load.sh
@@ -1,2 +1,2 @@
 docker stop boranga-dev
-docker run --rm --name=boranga-dev --env-file=env/boranga.env --hostname boranga-dev   --mount type=bind,source=/windata/,target=/windata/  --mount type=bind,source=/data/,target=/data/  -p 10.17.0.10:9000-9010:9000-9010 -p 10.17.0.10:2222:22 -p 10.17.0.10:7002:8080 -d -i -t dbcawa/docker_app_dev:boranga_dev_latest
+docker run --rm --name=boranga-dev --env-file=env/boranga.env --hostname boranga-dev   --mount type=bind,source=/windata/,target=/windata/  --mount type=bind,source=/data/,target=/data/  --mount type=bind,source=/data/data/dockerhome,target=/home/docker -p 10.17.0.10:9000-9010:9000-9010 -p 10.17.0.10:2222:22 -p 10.17.0.10:7002:8080 -d -i -t dbcawa/docker_app_dev:boranga_dev_latest
