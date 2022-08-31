67b7ade29a7d3134c699dff3cd201bf3c6cad10e
Jason Moore <jmoore@austwa.com>
2022-08-31 11:29:27 +0800

    Create gokart_sss_docker_load.sh

---
+++ windows_vbox/gokart_sss_docker_load.sh
@@ -0,0 +1,3 @@
+docker stop boranga-dev
+docker run --rm  --pull=always --name=boranga-dev --env-file=env/gokartsss.env --hostname gokartsss-dev  --mount type=bind,source=/data/data/dockerhome/,target=/home/docker/ --mount type=bind,source=/windata/,target=/windata/  --mount type=bind,source=/data/,target=/data/  -p 10.17.0.10:9020-9030:9020-9030 -p 10.17.0.10:2222:22 -p 10.17.0.10:7003:8080 -d -i -t dbcawa/gokart:2022.03.22.03.2649
+
