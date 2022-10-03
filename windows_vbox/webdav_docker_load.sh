c2724ddb61bd030f0278aef625b16e503d4a579a
Jason Moore <jason.moore@dbca.wa.gov.au>
2022-10-03 02:56:21 +0000

    Webdav Script Updates

--- windows_vbox/webdav_docker_load.sh
+++ windows_vbox/webdav_docker_load.sh
@@ -4,4 +4,4 @@ docker stop webdav
 docker rm webdav
 
 # specifically for a dev environment. - not for production use.
-docker run  --restart always -v /data:/var/lib/dav -e AUTH_TYPE=Digest -e USERNAME=docker -e PASSWORD=docker --publish 6999:80 --name webdav -e LOCATION=/webdav -d dbcawa/docker_app_dev:webdav_dev_latest 
+docker run  --pull=always  --restart always -v /data:/var/lib/dav -e AUTH_TYPE=Digest -e USERNAME=docker -e PASSWORD=dockerpass --publish 6999:80 --name webdav -e LOCATION=/webdav -d dbcawa/docker_app_dev:webdav_dev_2022.10.02.03.4834
