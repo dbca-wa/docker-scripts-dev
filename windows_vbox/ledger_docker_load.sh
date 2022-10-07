16c276acb119312787090d321922df77afadf609
Jason Moore <jmoore@austwa.com>
2022-10-07 12:11:40 +0800

    Update ledger_docker_load.sh

--- windows_vbox/ledger_docker_load.sh
+++ windows_vbox/ledger_docker_load.sh
@@ -1,2 +1,29 @@
-docker stop ledger-dev 
-docker run --rm --name=ledger-dev --env-file=env/ledger.env --hostname ledger-dev  --mount type=bind,source=/windata/,target=/windata/ -p 10.17.0.10:9021-9030:9021-9030 -p 10.17.0.10:2224:22 -p 10.17.0.10:7001:8080  -d -i -t dbcawa/docker_app_dev:ledger_dev_latest
+#!/bin/bash
+
+DOCKER_IMAGE="dbcawa/docker_app_dev:boranga_dev_latest"
+INSTANCE_NAME="ledger-dev"
+CONTAINER_WEB_PORT="7001"
+DEVPORTS="9021-9030"
+SSHPORT="2224"
+ENV_FILE="env/ledger.env"
+
+if [ "$1" ]
+then
+      DOCKER_IMAGE=$1
+      echo "Loading Image: $1"
+fi
+
+if [ "$2" ]
+then
+      ENV_FILE=$2
+      echo "Loading ENV: $2"
+fi
+
+
+docker stop $INSTANCE_NAME
+docker run --rm  --pull=always --name=$INSTANCE_NAME --env-file=$ENV_FILE --hostname $INSTANCE_NAME  --mount type=bind,source=/data/data/dockerhome/,target=/home/docker/ --mount type=bind,source=/windata/,target=/windata/  --mount type=bind,source=/data/,target=/data/  -p 10.17.0.10:$DEVPORTS:$DEVPORTS -p 10.17.0.10:$SSHPORT:22 -p 10.17.0.10:$CONTAINER_WEB_PORT:8080 -d -i -t $DOCKER_IMAGE
+
+
+
+#docker stop ledger-dev 
+#docker run --rm --name=ledger-dev --env-file=env/ledger.env --hostname ledger-dev  --mount type=bind,source=/windata/,target=/windata/ -p 10.17.0.10:9021-9030:9021-9030 -p 10.17.0.10:2224:22 -p 10.17.0.10:7001:8080  -d -i -t dbcawa/docker_app_dev:ledger_dev_latest
