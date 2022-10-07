157aa1adc417caac4a654750e0883ab3d8c30140
Jason Moore <jmoore@austwa.com>
2022-10-07 11:35:12 +0800

    Update boranga_docker_load.sh

--- windows_vbox/boranga_docker_load.sh
+++ windows_vbox/boranga_docker_load.sh
@@ -7,5 +7,13 @@ DEVPORTS="9000-9010"
 SSHPORT="2222"
 ENV_FILE="env/boranga.env"
 
+if [ -z "$1" ]
+then
+      echo "\$1 is empty"
+else
+      echo "\$1 is NOT empty"
+fi
+
+
 docker stop boranga-dev
 docker run --rm  --pull=always --name=$INSTANCE_NAME --env-file=$ENV_FILE --hostname $INSTANCE_NAME  --mount type=bind,source=/data/data/dockerhome/,target=/home/docker/ --mount type=bind,source=/windata/,target=/windata/  --mount type=bind,source=/data/,target=/data/  -p 10.17.0.10:$DEVPORTS:$DEVPORTS -p 10.17.0.10:$SSHPORT:22 -p 10.17.0.10:$CONTAINER_WEB_PORT:8080 -d -i -t $DOCKER_IMAGE
