729bfa576616451425e810a5a7384f529509ada1
Jason Moore <jmoore@austwa.com>
2022-10-07 11:41:38 +0800

    Update boranga_docker_load.sh

--- windows_vbox/boranga_docker_load.sh
+++ windows_vbox/boranga_docker_load.sh
@@ -13,6 +13,12 @@ then
       echo "Loading Image: $1"
 fi
 
+if [ "$2" ]
+then
+      ENV_FILE=$2
+      echo "Loading ENV: $2"
+fi
+
 
 docker stop boranga-dev
 docker run --rm  --pull=always --name=$INSTANCE_NAME --env-file=$ENV_FILE --hostname $INSTANCE_NAME  --mount type=bind,source=/data/data/dockerhome/,target=/home/docker/ --mount type=bind,source=/windata/,target=/windata/  --mount type=bind,source=/data/,target=/data/  -p 10.17.0.10:$DEVPORTS:$DEVPORTS -p 10.17.0.10:$SSHPORT:22 -p 10.17.0.10:$CONTAINER_WEB_PORT:8080 -d -i -t $DOCKER_IMAGE
