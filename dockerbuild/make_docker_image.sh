760d0e215e487138b42bd998fd4a7b2053777190
Jason Moore <jmoore@austwa.com>
2022-11-23 17:36:37 +0800

    Update make_docker_image.sh

--- dockerbuild/make_docker_image.sh
+++ dockerbuild/make_docker_image.sh
@@ -5,6 +5,7 @@ GIT_REPO=$1
 GIT_BRANCH=$2
 DOCKER_REPO=$3
 DOCKER_LATEST="latest"
+DOCKER_SPACER="_"
 docker login
 
 if [ -d "$HOME/gittmp" ]
@@ -17,6 +18,7 @@ then
   echo "Running git clone $GIT_REPO gittmp";
   cd $HOME;
   git clone $GIT_REPO "gittmp";
+  DOCKER_SPACER=""
 else
    echo "Running git clone $GIT_REPO -b $GIT_BRANCH gittmp";
    cd $HOME;
@@ -27,10 +29,10 @@ if [ -f "$HOME/gittmp/docker_repo_name" ]; then
      DOCKER_REPO=$(<$HOME/gittmp/docker_repo_name)
      echo $DOCKER_REPO;
      cd $HOME/gittmp/
-     docker image build  --tag  dbcawa/$DOCKER_REPO:$GIT_BRANCH$DOCKER_LATEST . ;
-     docker tag dbcawa/$DOCKER_REPO:latest dbcawa/$DOCKER_REPO:$DATE_VAR;
-     docker image push dbcawa/$DOCKER_REPO:$GIT_BRANCH$DOCKER_LATEST;
-     docker image push dbcawa/$DOCKER_REPO:$DATE_VAR;
+     docker image build  --tag  dbcawa/$DOCKER_REPO:$GIT_BRANCH$DOCKER_SPACER$DOCKER_LATEST . ;
+     docker tag dbcawa/$DOCKER_REPO:latest dbcawa/$DOCKER_REPO:$GIT_BRANCH$DOCKER_SPACER$DATE_VAR;
+     docker image push dbcawa/$DOCKER_REPO:$GIT_BRANCH$DOCKER_SPACER$DOCKER_LATEST;
+     docker image push dbcawa/$DOCKER_REPO:$GIT_BRANCH$DOCKER_SPACER$DATE_VAR;
 
 else
    echo "Docker Repo Name '$HOME/gittmp/docker_repo_name' file does not exist";
