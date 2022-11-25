1f4fe42dd34d4d0e6b3b13d3b827adfa12b6f09c
Jason Moore <jmoore@austwa.com>
2022-11-25 17:46:00 +0800

    Update make_docker_image.sh

--- dockerbuild/make_docker_image.sh
+++ dockerbuild/make_docker_image.sh
@@ -5,7 +5,6 @@ GIT_REPO=$1
 GIT_BRANCH=$2
 DOCKER_REPO=$3
 DOCKER_LATEST="latest"
-DOCKER_SPACER="_"
 docker login
 
 if [ -d "$HOME/gittmp" ]
@@ -18,7 +17,6 @@ then
   echo "Running git clone $GIT_REPO gittmp";
   cd $HOME;
   git clone $GIT_REPO "gittmp";
-  DOCKER_SPACER=""
 else
    echo "Running git clone $GIT_REPO -b $GIT_BRANCH gittmp";
    cd $HOME;
@@ -29,12 +27,33 @@ if [ -f "$HOME/gittmp/docker_repo_name" ]; then
      DOCKER_REPO=$(<$HOME/gittmp/docker_repo_name)
      echo $DOCKER_REPO;
      cd $HOME/gittmp/
-     docker image build  --tag  dbcawa/$DOCKER_REPO:$GIT_BRANCH$DOCKER_SPACER$DOCKER_LATEST . ;
-     docker tag dbcawa/$DOCKER_REPO:latest dbcawa/$DOCKER_REPO:$GIT_BRANCH$DOCKER_SPACER$DATE_VAR;
-     docker image push dbcawa/$DOCKER_REPO:$GIT_BRANCH$DOCKER_SPACER$DOCKER_LATEST;
-     docker image push dbcawa/$DOCKER_REPO:$GIT_BRANCH$DOCKER_SPACER$DATE_VAR;
+     docker image build  --tag  dbcawa/$DOCKER_REPO:$GIT_BRANCH$DOCKER_LATEST . ;
+     docker tag dbcawa/$DOCKER_REPO:latest dbcawa/$DOCKER_REPO:$DATE_VAR;
+     docker image push dbcawa/$DOCKER_REPO:$GIT_BRANCH$DOCKER_LATEST;
+     docker image push dbcawa/$DOCKER_REPO:$DATE_VAR;
 
 else
    echo "Docker Repo Name '$HOME/gittmp/docker_repo_name' file does not exist";
    exit 1
+fi
+
+if [ -f "$HOME/gittmp/dockerdev/Dockerfile" ]; then
+    cd $HOME/gittmp/dockerdev/;
+    dockerrepo="docker_app_dev"
+    seperator="_"
+    dockersubtagdev="_dev"
+    dockersubtag="$DOCKER_REPO$dockersubtagdev"
+    latest="latest"
+    docker image build   --tag  dbcawa/$dockerrepo:$dockersubtag$seperator$latest . ;
+    docker tag dbcawa/$dockerrepo:$dockersubtag$seperator$latest dbcawa/$dockerrepo:$dockersubtag$seperator$DATE_VAR;
+    docker image push dbcawa/$dockerrepo:$dockersubtag$seperator$latest;
+    docker image push dbcawa/$dockerrepo:$dockersubtag$seperator$DATE_VAR;
+fi
+echo "Docker Images";
+echo "dbcawa/$DOCKER_REPO:$GIT_BRANCH$DOCKER_LATEST";
+echo "dbcawa/$DOCKER_REPO:$DATE_VAR";
+if [ -f "$HOME/gittmp/dockerdev/Dockerfile" ]; then
+   echo "Docker Dev Images";
+   echo "dbcawa/$dockerrepo:$dockersubtag$seperator$latest";
+   echo "dbcawa/$dockerrepo:$dockersubtag$seperator$DATE_VAR";
 fi
