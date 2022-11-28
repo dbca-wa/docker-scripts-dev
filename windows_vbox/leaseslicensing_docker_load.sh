bfad03bd5a22f064f9d095cbda9e0b6d3c47af17
Jason Moore <jmoore@austwa.com>
2022-11-28 15:54:00 +0800

    Create leaseslicensing_docker_load.sh

---
+++ windows_vbox/leaseslicensing_docker_load.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DOCKER_IMAGE="dbcawa/docker_app_dev:leaseslicensing_dev_latest"
+INSTANCE_NAME="leaseslicensing-dev"
+CONTAINER_WEB_PORT="7008"
+DEVPORTS="9071-9080"
+SSHPORT="2228"
+ENV_FILE="env/leaseslicensing.env"
+
+if [ "$1" ]
+then
+      DOCKER_IMAGE=$1
+      echo "Loading Image: $1"
+fi
+
+if [ "$2" ]
+then
+      ENV_FILE=$2
+      echo "Loading ENV: $2"
+fi
+
+
+docker stop $INSTANCE_NAME
+docker run --rm  --pull=always --name=$INSTANCE_NAME --env-file=$ENV_FILE --hostname $INSTANCE_NAME  --mount type=bind,source=/data/data/dockerhome/,target=/home/docker/ --mount type=bind,source=/windata/,target=/windata/  --mount type=bind,source=/data/,target=/data/  -p 10.17.0.10:$DEVPORTS:$DEVPORTS -p 10.17.0.10:$SSHPORT:22 -p 10.17.0.10:$CONTAINER_WEB_PORT:8080 -d -i -t $DOCKER_IMAGE
