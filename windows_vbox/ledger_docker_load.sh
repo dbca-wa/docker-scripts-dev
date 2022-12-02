dd840d309e2436f80359a789c20d5832ef77ce7f
Jason Moore <jmoore@austwa.com>
2022-12-02 12:32:18 +0800

    Update ledger_docker_load.sh

--- windows_vbox/ledger_docker_load.sh
+++ windows_vbox/ledger_docker_load.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
 DOCKER_IMAGE="dbcawa/docker_app_dev:ledger_dev_latest"
 INSTANCE_NAME="ledger-dev"
 CONTAINER_WEB_PORT="7001"
 DEVPORTS="9021-9030"
 SSHPORT="2224"
-ENV_FILE="env/ledger.env"
+ENV_FILE="/env/ledger.env"
 
 if [ "$1" ]
 then
@@ -21,7 +23,7 @@ fi
 
 
 docker stop $INSTANCE_NAME
-docker run --rm  --pull=always --name=$INSTANCE_NAME --env-file=$ENV_FILE --hostname $INSTANCE_NAME  --mount type=bind,source=/data/data/dockerhome/,target=/home/docker/ --mount type=bind,source=/windata/,target=/windata/  --mount type=bind,source=/data/,target=/data/  -p 10.17.0.10:$DEVPORTS:$DEVPORTS -p 10.17.0.10:$SSHPORT:22 -p 10.17.0.10:$CONTAINER_WEB_PORT:8080 -d -i -t $DOCKER_IMAGE
+docker run --rm  --pull=always --name=$INSTANCE_NAME --env-file=$SCRIPTPATH$ENV_FILE --hostname $INSTANCE_NAME  --mount type=bind,source=/data/data/dockerhome/,target=/home/docker/ --mount type=bind,source=/windata/,target=/windata/  --mount type=bind,source=/data/,target=/data/  -p 10.17.0.10:$DEVPORTS:$DEVPORTS -p 10.17.0.10:$SSHPORT:22 -p 10.17.0.10:$CONTAINER_WEB_PORT:8080 -d -i -t $DOCKER_IMAGE
 
 
 
