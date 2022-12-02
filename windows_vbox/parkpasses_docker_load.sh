cb2ef4b383c6628ec25944b559aadfde3c502b1e
Jason Moore <jmoore@austwa.com>
2022-12-02 12:29:23 +0800

    Update parkpasses_docker_load.sh

--- windows_vbox/parkpasses_docker_load.sh
+++ windows_vbox/parkpasses_docker_load.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
 DOCKER_IMAGE="dbcawa/docker_app_dev:park-passes_dev_latest"
 INSTANCE_NAME="parkpasses-dev"
 CONTAINER_WEB_PORT="7009"
 DEVPORTS="9081-9090"
 SSHPORT="2229"
-ENV_FILE="env/parkpasses.env"
+ENV_FILE="/env/parkpasses.env"
 
 if [ "$1" ]
 then
@@ -21,4 +23,4 @@ fi
 
 
 docker stop $INSTANCE_NAME
-docker run --rm  --pull=always --name=$INSTANCE_NAME --env-file=$ENV_FILE --hostname $INSTANCE_NAME  --mount type=bind,source=/data/data/dockerhome/,target=/home/docker/ --mount type=bind,source=/windata/,target=/windata/  --mount type=bind,source=/data/,target=/data/  -p 10.17.0.10:$DEVPORTS:$DEVPORTS -p 10.17.0.10:$SSHPORT:22 -p 10.17.0.10:$CONTAINER_WEB_PORT:8080 -d -i -t $DOCKER_IMAGE
+docker run --rm  --pull=always --name=$INSTANCE_NAME --env-file=$SCRIPTPATH$ENV_FILE --hostname $INSTANCE_NAME  --mount type=bind,source=/data/data/dockerhome/,target=/home/docker/ --mount type=bind,source=/windata/,target=/windata/  --mount type=bind,source=/data/,target=/data/  -p 10.17.0.10:$DEVPORTS:$DEVPORTS -p 10.17.0.10:$SSHPORT:22 -p 10.17.0.10:$CONTAINER_WEB_PORT:8080 -d -i -t $DOCKER_IMAGE
