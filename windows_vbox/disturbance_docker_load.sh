daf89b3055e18284a9aff3ed96876eff74cacd02
Jason Moore <jmoore@austwa.com>
2022-12-02 12:30:37 +0800

    Update disturbance_docker_load.sh

--- windows_vbox/disturbance_docker_load.sh
+++ windows_vbox/disturbance_docker_load.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
 DOCKER_IMAGE="dbcawa/docker_app_dev:disturbance_dev_2022.10.12.07.0541"
 INSTANCE_NAME="disturbance-dev"
 CONTAINER_WEB_PORT="7007"
 DEVPORTS="9061-9070"
 SSHPORT="2227"
-ENV_FILE="env/disturbance.env"
+ENV_FILE="/env/disturbance.env"
 
 if [ "$1" ]
 then
@@ -20,4 +22,4 @@ then
 fi
 
 docker stop $INSTANCE_NAME
-docker run --rm  --pull=always --name=$INSTANCE_NAME --env-file=$ENV_FILE --hostname $INSTANCE_NAME  --mount type=bind,source=/data/data/dockerhome/,target=/home/docker/ --mount type=bind,source=/windata/,target=/windata/  --mount type=bind,source=/data/,target=/data/  -p 10.17.0.10:$DEVPORTS:$DEVPORTS -p 10.17.0.10:$SSHPORT:22 -p 10.17.0.10:$CONTAINER_WEB_PORT:8080 -d -i -t $DOCKER_IMAGE
+docker run --rm  --pull=always --name=$INSTANCE_NAME --env-file=$SCRIPTPATH$ENV_FILE --hostname $INSTANCE_NAME  --mount type=bind,source=/data/data/dockerhome/,target=/home/docker/ --mount type=bind,source=/windata/,target=/windata/  --mount type=bind,source=/data/,target=/data/  -p 10.17.0.10:$DEVPORTS:$DEVPORTS -p 10.17.0.10:$SSHPORT:22 -p 10.17.0.10:$CONTAINER_WEB_PORT:8080 -d -i -t $DOCKER_IMAGE
