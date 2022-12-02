3236d9e37b443cc723352d95cb403f3630cf4ad2
Jason Moore <jmoore@austwa.com>
2022-12-02 12:33:47 +0800

    Update wildlifelegacy_docker_load.sh

--- windows_vbox/wildlifelegacy_docker_load.sh
+++ windows_vbox/wildlifelegacy_docker_load.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
 DOCKER_IMAGE="dbcawa/docker_app_dev:wildlifelegacy_dev_2022.10.03.09.3018"
 INSTANCE_NAME="wildlifelegacy-dev"
 CONTAINER_WEB_PORT="7005"
 DEVPORTS="9041-9050"
 SSHPORT="2225"
-ENV_FILE="env/wildlifelegacy.env"
+ENV_FILE="/env/wildlifelegacy.env"
 
 if [ "$1" ]
 then
@@ -20,7 +22,7 @@ then
 fi
 
 docker stop $INSTANCE_NAME
-docker run --rm  --pull=always --name=$INSTANCE_NAME --env-file=$ENV_FILE --hostname $INSTANCE_NAME  --mount type=bind,source=/data/data/dockerhome/,target=/home/docker/ --mount type=bind,source=/windata/,target=/windata/  --mount type=bind,source=/data/,target=/data/  -p 10.17.0.10:$DEVPORTS:$DEVPORTS -p 10.17.0.10:$SSHPORT:22 -p 10.17.0.10:$CONTAINER_WEB_PORT:8080 -d -i -t $DOCKER_IMAGE
+docker run --rm  --pull=always --name=$INSTANCE_NAME --env-file=$SCRIPTPATH$ENV_FILE --hostname $INSTANCE_NAME  --mount type=bind,source=/data/data/dockerhome/,target=/home/docker/ --mount type=bind,source=/windata/,target=/windata/  --mount type=bind,source=/data/,target=/data/  -p 10.17.0.10:$DEVPORTS:$DEVPORTS -p 10.17.0.10:$SSHPORT:22 -p 10.17.0.10:$CONTAINER_WEB_PORT:8080 -d -i -t $DOCKER_IMAGE
 
 
 
