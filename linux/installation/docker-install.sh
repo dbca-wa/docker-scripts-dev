3f1120313575f4438f248b03ff15598bb7c57e3e
Jason Moore <jmoore@austwa.com>
2023-05-14 21:36:30 +0800

    Update docker-install.sh

--- linux/installation/docker-install.sh
+++ linux/installation/docker-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-sudo apt install openssh-server
+sudo apt install openssh-server -y
 sudo apt install apt-transport-https curl gnupg-agent ca-certificates software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
