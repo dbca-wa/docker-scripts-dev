5774e2bc7ef52741d734d33db9d195bef475bff1
Jason Moore <jmoore@austwa.com>
2025-02-21 16:40:04 +0800

    Create postgres_docker_load_2404_16.sh

---
+++ windows_vbox/postgres_docker_load_2404_16.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [ -d "/data" ]
+then
+   echo "Directory Exists /data"
+else
+   sudo mkdir "/data"
+fi
+
+if [ -d "/linuxdata" ]
+then
+   echo "Directory Exists /linuxdata"
+else
+   sudo mkdir "/linuxdata"
+fi
+
+if [ -d "/linuxdata/postgres2404-dev-16/" ]
+then
+   echo "Directory Exists /linuxdata/postgres2404-dev-16/"
+else
+   sudo mkdir "/linuxdata/postgres2404-dev-16/"
+fi
+
+if [ -d "/linuxdata/postgres2404-dev-16/data" ]
+then
+   echo "Directory Exists /linuxdata/postgres2404-dev-16/data"
+else
+   sudo mkdir "/linuxdata/postgres2404-dev-16/data"
+fi
+
+if [ -d "/windata" ]
+then
+   echo "Directory Exists /windata"
+else
+   sudo mkdir "/windata"
+fi
+
+docker stop postgres2404-dev-16
+docker run --rm --pull=always --name=postgres2404-dev --hostname postgres2404-dev-16 --mount type=bind,source=/linuxdata/postgres2404-dev-16/data/,target=/data/ --mount type=bind,source=/data/,target=/datadev  -p 25432:5432  -d -i -t ghcr.io/dbca-wa/docker-apps-dev:postgresubuntu2404_16_latest
