c46b9ab1b699fe9c0474c5d2e85d10e626494134
Jason Moore <jmoore@austwa.com>
2025-04-22 16:54:55 +0800

    Create pbs_docker_load.sh

---
+++ windows_vbox/pbs_docker_load.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
+DOCKER_IMAGE="dbcawa/docker_app_dev:pbs_dev_2025.04.15.04.2232"
+INSTANCE_NAME="pbs-legacy-dev"
+CONTAINER_WEB_PORT="10030"
+DEVPORTS="10201-10205"
+ENV_FILE="/env/bfrs_dfes_wrapper.env"
+
+if [ "$1" ]
+then
+      DOCKER_IMAGE=$1
+      echo "Loading Image: $1"
+fi
+
+if [ "$2" ]
+then
+      ENV_FILE=$2
+      echo "Loading ENV: $2"
+fi
+
+if [ -d "/data/data/containerhome/" ]
+then
+   echo "Directory Exists /data/data/containerhome/"
+else
+   sudo mkdir "/data/data/containerhome/"
+fi
+
+
+docker stop $INSTANCE_NAME
+docker run --rm  --pull=always --name=$INSTANCE_NAME --env-file=$SCRIPTPATH$ENV_FILE --hostname $INSTANCE_NAME --mount type=bind,source=/data/data/containerhome/,target=/home/container/ --mount type=bind,source=/windata/,target=/windata/  --mount type=bind,source=/data/,target=/data/  -p 10.17.0.10:$DEVPORTS:$DEVPORTS -p 10.17.0.10:$CONTAINER_WEB_PORT:8080 -d -i -t $DOCKER_IMAGE
