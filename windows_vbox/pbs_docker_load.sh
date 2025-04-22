c13ca1fc2b28aeb143b9e3ddc270516776dedd3e
Jason Moore <jmoore@austwa.com>
2025-04-22 17:09:31 +0800

    Update pbs_docker_load.sh

--- windows_vbox/pbs_docker_load.sh
+++ windows_vbox/pbs_docker_load.sh
@@ -2,7 +2,8 @@
 
 SCRIPT=$(readlink -f $0)
 SCRIPTPATH=`dirname $SCRIPT`
-DOCKER_IMAGE="dbcawa/docker_app_dev:2024.10.04.03.2105"
+#DOCKER_IMAGE="dbcawa/docker_app_dev:2024.10.04.03.2105"
+DOCKER_IMAGE="dbcawa/pbs:2024.10.04.03.2105"
 INSTANCE_NAME="pbs-legacy-dev"
 CONTAINER_WEB_PORT="10030"
 DEVPORTS="10201-10205"
