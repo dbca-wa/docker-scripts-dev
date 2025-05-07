ba5c08c9135b31665018a1ee32e3599d2aede6d3
Jason Moore <jason.moore@dbca.wa.gov.au>
2025-05-07 22:02:19 +0800

    Kubectl Script

---
+++ kubectl/stop_all_workloads.py
@@ -0,0 +1,15 @@
+import subprocess
+import os
+
+kubectl_cmd = "kubectl.exe"
+namespace = "jason"
+
+if os.name == 'posix':
+    kubectl_cmd = "kubectl"
+    git_cmd = "git"
+
+try:
+    subprocess.run([kubectl_cmd, "scale", "--replicas=0", "--all", "deployments","--namespace="+namespace]) 
+except Exception as e:
+    print ("Error: scaling deployment down")
+    print (e)
