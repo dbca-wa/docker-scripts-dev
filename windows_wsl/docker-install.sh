a7eacbc51eb2722f3b84907ad0044356c9d8426d
Jason Moore <jmoore@austwa.com>
2025-10-01 12:33:47 +0800

    Update docker-install.sh

--- windows_wsl/docker-install.sh
+++ windows_wsl/docker-install.sh
@@ -4,6 +4,7 @@ sudo apt install apt-transport-https curl gnupg-agent ca-certificates software-p
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt install docker-ce docker-ce-cli containerd.io -y
+sudo apt install ssh
 
 newgrp docker
 docker version
